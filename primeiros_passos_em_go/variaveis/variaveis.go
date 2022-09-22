f1427f5e6987a5251815e899d9d494a0b1291103
Fabio Sousa <clubeteczagem@gmail.com>
2022-09-22 05:40:57 -0300

    endereço de memória

--- primeiros_passos_em_go/variaveis/variaveis.go
+++ primeiros_passos_em_go/variaveis/variaveis.go
@@ -1,17 +1,23 @@
 package main
 
 import 	"fmt"
-import	"reflect"
+
 
 func main(){
 	nome := "Fabio" 
 	versao := 1.1
 
 	fmt.Println("Olá sr.", nome)
-	fmt.Println("Versão", versao)
+	fmt.Println("Este programa está na versão: ", versao)
 
-	fmt.Println("O tipo da variável nome é :", reflect.TypeOf(nome))
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("3- Sair dos programas")
 
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println("O endereço da variável comando é",&comando)
+	fmt.Println("O caomando escolhido foi", comando)
 
 	
 }
