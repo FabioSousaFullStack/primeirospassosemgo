e2a18b601e21e9b3332d30e9b23e6223fd794561
Fabio Sousa <clubeteczagem@gmail.com>
2022-10-04 20:49:52 -0300

    contrario

--- primeiros_passos_em_go/defer/defer.go
+++ primeiros_passos_em_go/defer/defer.go
@@ -3,11 +3,74 @@ package main
 import "fmt"
 
 func main(){
-	
-
-	for i := 0; i < 10; i++{
-		defer fmt.Println(i)
-	}
-
-	fmt.Println("I'm last in first out ")
+	contrario()
 }
+
+func texto(){
+	
+	fmt.Println("Não te amo mais.")
+	fmt.Println("Estarei mentindo dizendo que")
+	fmt.Println("Ainda te quero como sempre quis.")
+	fmt.Println("Tenho certeza que")
+	fmt.Println("Nada foi em vão.")
+	fmt.Println("Sinto dentro de mim que")
+	fmt.Println("Você não significa nada.")
+	fmt.Println("Não poderia dizer jamais que")
+	fmt.Println("Alimento um grande amor.")
+	fmt.Println("Sinto cada vez mais que")
+	fmt.Println("Já te esqueci!")
+	fmt.Println("E jamais usarei a frase:")
+	fmt.Println("EU TE AMO!")
+	fmt.Println("Sinto, mas tenho que dizer a verdade")
+	fmt.Println("É tarde demais...")
+
+}
+
+func contrario(){
+	
+	defer fmt.Println("Não te amo mais.")
+	defer fmt.Println("Estarei mentindo dizendo que")
+	defer fmt.Println("Ainda te quero como sempre quis.")
+	defer fmt.Println("Tenho certeza que")
+	defer fmt.Println("Nada foi em vão.")
+	defer fmt.Println("Sinto dentro de mim que")
+	defer fmt.Println("Você não significa nada.")
+	defer fmt.Println("Não poderia dizer jamais que")
+	defer fmt.Println("Alimento um grande amor.")
+	defer fmt.Println("Sinto cada vez mais que")
+	defer fmt.Println("Já te esqueci!")
+	defer fmt.Println("E jamais usarei a frase:")
+	defer fmt.Println("EU TE AMO!")
+	defer fmt.Println("Sinto, mas tenho que dizer a verdade")
+	defer fmt.Println("É tarde demais...")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
