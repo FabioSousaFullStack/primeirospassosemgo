f7b28ecfa7fb7f0a753bcf43d0cd0784f3b69d2b
Fabio Sousa <clubeteczagem@gmail.com>
2022-10-04 21:41:42 -0300

    iniciando a configuração de texto

--- primeiros_passos_em_go/defer/defer.go
+++ primeiros_passos_em_go/defer/defer.go
@@ -3,52 +3,32 @@ package main
 import "fmt"
 
 func main(){
+
+texto := [15]string{
+	"É tarde demais...",
+	"Estarei mentindo dizendo que",
+	"Ainda te quero como sempre quis.",
+	"Tenho certeza que",
+	"Nada foi em vão.",
+	"Sinto dentro de mim que",
+	"Você não significa nada.",
+	"Não poderia dizer jamais que",
+	"Alimento um grande amor.",
+	"Sinto cada vez mais que",
+	"Já te esqueci!",
+	"E jamais usarei a frase:",
+	"EU TE AMO!",
+	"Sinto, mas tenho que dizer a verdade",	
+	"É tarde demais...",
+
+}
+
 	fmt.Println("Este é um texto escrito por Clarice Lispector")
 	fmt.Println("Não te amo mais")
-
-	contrario()
 	 
 }
 
-func texto(){
-	
-	fmt.Println("Não te amo mais.")
-	fmt.Println("Estarei mentindo dizendo que")
-	fmt.Println("Ainda te quero como sempre quis.")
-	fmt.Println("Tenho certeza que")
-	fmt.Println("Nada foi em vão.")
-	fmt.Println("Sinto dentro de mim que")
-	fmt.Println("Você não significa nada.")
-	fmt.Println("Não poderia dizer jamais que")
-	fmt.Println("Alimento um grande amor.")
-	fmt.Println("Sinto cada vez mais que")
-	fmt.Println("Já te esqueci!")
-	fmt.Println("E jamais usarei a frase:")
-	fmt.Println("EU TE AMO!")
-	fmt.Println("Sinto, mas tenho que dizer a verdade")
-	fmt.Println("É tarde demais...")
 
-}
-
-func contrario(){
-	
-	defer fmt.Println("Não te amo mais.")
-	defer fmt.Println("Estarei mentindo dizendo que")
-	defer fmt.Println("Ainda te quero como sempre quis.")
-	defer fmt.Println("Tenho certeza que")
-	defer fmt.Println("Nada foi em vão.")
-	defer fmt.Println("Sinto dentro de mim que")
-	defer fmt.Println("Você não significa nada.")
-	defer fmt.Println("Não poderia dizer jamais que")
-	defer fmt.Println("Alimento um grande amor.")
-	defer fmt.Println("Sinto cada vez mais que")
-	defer fmt.Println("Já te esqueci!")
-	defer fmt.Println("E jamais usarei a frase:")
-	defer fmt.Println("EU TE AMO!")
-	defer fmt.Println("Sinto, mas tenho que dizer a verdade")
-	defer fmt.Println("É tarde demais...")
-
-}
 
 
 
